4909acbd0f9d4267c15636dad09f8cf16c22b0d5
Mathias Meiser <mathias@Mathiass-MacBook-Pro-2.fritz.box>
2016-06-11 17:53:42 +0200

    listbinding

--- resources/sap/ui/model/firebase/FirebaseModel.js
+++ resources/sap/ui/model/firebase/FirebaseModel.js
@@ -7,8 +7,8 @@
  */
 
 // Provides the Firebase backend based model implementation
-sap.ui.define(['jquery.sap.global', 'sap/ui/model/ClientModel', 'sap/ui/model/Context', './FirebasePropertyBinding'],
-	function(jQuery, ClientModel, Context, FirebasePropertyBinding) {
+sap.ui.define(['jquery.sap.global', 'sap/ui/model/ClientModel', 'sap/ui/model/Context', 'sap/ui/model/json/JSONPropertyBinding', 'sap/ui/model/json/JSONListBinding'],
+	function(jQuery, ClientModel, Context, JSONPropertyBinding, JSONListBinding) {
 	"use strict";
 
 
@@ -23,20 +23,17 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/ClientModel', 'sap/ui/model/Co
 	 * @author SAP SE
 	 * @version 1.38.3
 	 *
-	 * @param {string} sRef Reference to Firebase Realtime database
+	 * @param {object} config for Firebase Realtime database
 	 * @constructor
 	 * @public
 	 * @alias sap.ui.model.firebase.FirebaseModel
 	 */
 	var FirebaseModel = ClientModel.extend("sap.ui.model.firebase.FirebaseModel", /** @lends sap.ui.model.firebase.FirebaseModel.prototype */ {
 
-		constructor : function(sRef) {
+		constructor : function(config) {
 			//this.pSequentialImportCompleted = Promise.resolve();
 			ClientModel.apply(this, arguments);
-
-			if (sRef) {
-				this.setRef(sRef);
-			}
+			firebase.initializeApp(config);
 		},
 
 		metadata : {
@@ -49,23 +46,40 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/ClientModel', 'sap/ui/model/Co
 	 * Sets the Firebase backend data to the model.
 	 *
 	 * @param {string} sRef Reference to Firebase Realtime database
+	 * @param {boolean} bIsNode Distinguishes between nodes and leafs in Firebase database model
 	 *
 	 * @public
 	 */
-	FirebaseModel.prototype.setRef = function(sRef){
+	FirebaseModel.prototype.addRef = function(sRef,bIsNode){
 
-		var that = this;		
-		firebase.database().ref(sRef).on('value', function(snapshot) {
-			// TODO: Resolution of JSON Object structure from string sRef
-			that.oData = snapshot.val();
-			that.checkUpdate();
-		});
+		var that = this;	
+		if (!bIsNode) {	
+			firebase.database().ref(sRef).on('value', function(snapshot) {
+				that.oData[sRef] = snapshot.val();
+				that.checkUpdate();
+			});
+		} 
+		else {
+			this.oData[sRef] = {};
+			firebase.database().ref(sRef).on('child_changed', function(data) {
+				that.oData[sRef][data.key] = data.val();
+				that.checkUpdate();
+			});
+			firebase.database().ref(sRef).on('child_added', function(data) {
+				that.oData[sRef][data.key] = data.val();
+				that.checkUpdate();
+			});
+			firebase.database().ref(sRef).on('child_removed', function(data) {
+				that.oData[sRef][data.key] = null;
+				that.checkUpdate();
+			});
+		}
 	};
 
 
 
 	FirebaseModel.prototype.loadData = function(sURL, oParameters, bAsync, sType, bMerge, bCache, mHeaders){
-		this.setRef(sURL);
+		
 	};
 
 
@@ -74,7 +88,18 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/ClientModel', 'sap/ui/model/Co
 	 *
 	 */
 	FirebaseModel.prototype.bindProperty = function(sPath, oContext, mParameters) {
-		var oBinding = new FirebasePropertyBinding(this, sPath, oContext, mParameters);
+		this.addRef(this.resolve(sPath,oContext), mParameters ? mParameters.isNode : null);
+		var oBinding = new JSONPropertyBinding(this, sPath, oContext, mParameters);
+		return oBinding;
+	};
+
+	/**
+	 * @see sap.ui.model.Model.prototype.bindList
+	 *
+	 */
+	FirebaseModel.prototype.bindList = function(sPath, oContext, aSorters, aFilters, mParameters) {
+		this.addRef(this.resolve(sPath,oContext), true);
+		var oBinding = new JSONListBinding(this, sPath, oContext, aSorters, aFilters, mParameters);
 		return oBinding;
 	};
 	
@@ -107,33 +132,15 @@ sap.ui.define(['jquery.sap.global', 'sap/ui/model/ClientModel', 'sap/ui/model/Co
 		return this._getObject(sPath, oContext);
 	};
 
-	/**
-	 * @param {string} sPath
-	 * @param {object} [oContext]
-	 * @returns {any} the node of the specified path/context
-	 */
-	FirebaseModel.prototype._getObject = function (sPath, oContext) {
-		var oNode = this.isLegacySyntax() ? this.oData : null;
-		if (oContext instanceof Context) {
-			oNode = this._getObject(oContext.getPath());
-		} else if (oContext) {
-			oNode = oContext;
-		}
-		if (!sPath) {
-			return oNode;
-		}
-		var aParts = sPath.split("/"),
-			iIndex = 0;
-		if (!aParts[0]) {
-			// absolute path starting with slash
-			oNode = this.oData;
-			iIndex++;
-		}
-		while (oNode && aParts[iIndex]) {
-			oNode = oNode[aParts[iIndex]];
-			iIndex++;
-		}
-		return oNode;
+	FirebaseModel.prototype._getObject = function(sPath, oContext) {
+		return this.oData[this.resolve(sPath, oContext)];
+	};
+
+
+
+
+	FirebaseModel.prototype.isList = function(sPath, oContext) {
+		return jQuery.isArray(this._getObject(sPath, oContext));
 	};
 
 	return FirebaseModel;
